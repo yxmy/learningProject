7aef5fe6657e43283a84fecd3eac378eddae9d4d
yxmy <laopo5201314>
2019-03-26 23:55:35 +0800

    算法

---
+++ src/main/java/com/yx/springboot/demospring2/datastructureandalgorithm/ch02/SelectionSort.java
@@ -0,0 +1,24 @@
+package com.yx.springboot.demospring2.datastructureandalgorithm.ch02;
+
+/**
+ * 选择排序
+ */
+public class SelectionSort {
+
+    public static void sort(int [] arr){
+        int k = 0;
+        int tmp = 0;
+        for(int i=0;i<arr.length-1;i++){
+            k = i;
+            for(int j = i; j <arr.length; j++){
+                if(arr[j] < arr[k]){
+                    k = j;
+                }
+            }
+            tmp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = tmp;
+        }
+    }
+
+}
