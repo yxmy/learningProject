b0c0cddeec3091da825ef375b502e3cb3e33be96
袁鑫 <yuanxin@seeyon.com>
2021-05-22 15:39:42 +0800

    leetCode算法题

---
+++ src/main/java/com/yx/springboot/demospring/leetcode/RemoveDuplicates.java
@@ -0,0 +1,55 @@
+package com.yx.springboot.demospring.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 说明:
+ * <p>
+ * 为什么返回数值是整数，但输出的答案是数组呢?
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+ *
+ * @author yuanxin
+ * @date 2021/5/20
+ */
+public class RemoveDuplicates {
+
+    /**
+     * 定义两个指针，fast和slow，fast记录遍历到的位置，slow记录下一个元素要放的位置
+     * 从1开始，如果fast当前坐标数值和它前一位的数值不相等，把当前fast指向的数值移动到slow的位置，然后slow++
+     * 如果相等，则fast继续向后面找
+     *
+     */
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return nums.length;
+        }
+        int fast = 1;
+        int slow = 1;
+        while (fast < nums.length) {
+            final int currFastNum = nums[fast];
+            final int preFastNum = nums[fast - 1];
+            if (currFastNum != preFastNum) {
+                nums[slow] = currFastNum;
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        final int i = removeDuplicates(nums);
+        System.out.println(i);
+        for (int j = 0; j < i; j++) {
+            System.out.print(nums[j] + ",");
+        }
+    }
+}
---
+++ src/main/java/com/yx/springboot/demospring/leetcode/RemoveElement.java
@@ -0,0 +1,39 @@
+package com.yx.springboot.demospring.leetcode;
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ * @author yuanxin
+ * @date 2021/5/21
+ */
+public class RemoveElement {
+
+    /**
+     * fast遍历，slow记录下一个不等于val的值存放的位置
+     */
+    public static int removeElement(int[] nums, int val) {
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,1,1,1};
+        final int length = removeElement(nums, 1);
+        System.out.println(length);
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
+
+
+}
