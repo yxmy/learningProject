65d946f35ecf5498352b3b1907a55a49b2611c22
yxmy <yuanxin@seeyon.com>
2021-09-06 18:34:38 +0800

    Java8

--- src/main/java/com/yx/springboot/demospring/java8/optional/OptionalTest.java
+++ src/main/java/com/yx/springboot/demospring/java8/optional/OptionalTest.java
@@ -1,9 +1,15 @@
 package com.yx.springboot.demospring.java8.optional;
 
+import com.yx.springboot.demospring.java8.entity.Car;
+import com.yx.springboot.demospring.java8.entity.Insurance;
+import com.yx.springboot.demospring.java8.entity.OptionalCar;
+import com.yx.springboot.demospring.java8.entity.OptionalPerson;
+import com.yx.springboot.demospring.java8.entity.Person;
 import com.yx.springboot.demospring.testlist.model.User;
 import org.junit.Test;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 /**
  * @author Yuan_xin
@@ -19,5 +25,27 @@ public class OptionalTest {
         Optional<User> user2 = Optional.ofNullable(user);
 
         Optional.ofNullable(user).orElseGet(User::new);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        OptionalPerson optionalPerson = new OptionalPerson();
+        final Optional<Optional<OptionalCar>> car = Optional.ofNullable(optionalPerson).map(OptionalPerson::getCar);
+        final Optional<OptionalCar> car1 = Optional.ofNullable(optionalPerson).flatMap(OptionalPerson::getCar);
+        final String unKnown = Optional.of(optionalPerson)
+                .flatMap(OptionalPerson::getCar)
+                .flatMap(OptionalCar::getInsurance)
+                .map(Insurance::getName).orElse("unKnown");
+
+        Person person = new Person();
+        final String s = Optional.ofNullable(person).map(Person::getCar)
+                .map(Car::getInsurance).map(Insurance::getName).orElse("unKnown");
+
+        final String s1 = Optional.ofNullable(person).map(Person::getCar)
+                .map(Car::getInsurance).map(Insurance::getName).orElseGet(() -> "unKnown");
+
+        Optional.ofNullable(person).map(Person::getCar)
+                .map(Car::getInsurance).map(Insurance::getName).orElseThrow((Supplier<Throwable>) RuntimeException::new);
+
     }
 }
