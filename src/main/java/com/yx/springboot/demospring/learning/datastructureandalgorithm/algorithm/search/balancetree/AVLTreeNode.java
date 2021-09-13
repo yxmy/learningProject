39db37fa0f5396e5eceb08f97145bda21aafff98
yxmy <yuanxin@starwillsoft.com>
2021-09-13 23:41:35 +0800

    平衡二叉树

---
+++ src/main/java/com/yx/springboot/demospring/learning/datastructureandalgorithm/algorithm/search/balancetree/AVLTreeNode.java
@@ -0,0 +1,18 @@
+package com.yx.springboot.demospring.learning.datastructureandalgorithm.algorithm.search.balancetree;
+
+/**
+ * 平衡二叉树结点
+ *
+ * @author yuanxin
+ * @date 2021/9/13.
+ */
+public class AVLTreeNode {
+    /**数据 */
+    int data;
+    /** 当前结点的平衡因子 */
+    int bf;
+    /** 左孩子 */
+    AVLTreeNode lChild;
+    /** 右孩子 */
+    AVLTreeNode rChild;
+}
